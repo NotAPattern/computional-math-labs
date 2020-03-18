9fb2ce0a1876b6ff148821d65a253aae6175cfbc
Nikita Karatsev <nikita.karatsev@gmail.com>
2020-03-18 19:05:09 +0600

    [~]main.cpp

--- second-semester/nastya/main.cpp
+++ second-semester/nastya/main.cpp
@@ -6,13 +6,34 @@
 
 int main() {
     Matrix A(N, N);
-    Matrix At(N, N);
-    Matrix Acomposion(N, N);
+    Matrix Aiter(N, N);
+    Matrix B(N, 1);
+    double tetta = 1.4;
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){ // n=m
             std::cin >> A(i, j);
         }
     }
+    std::cout << std::endl;
+    std::cout << "Input vector b: ";
+    for(int j = 0; j < N; j++){ // n=m
+        std::cin >> B(j, 1);
+    }
+    std::cout << "Input tetta(iter parametr): ";
+    std::cin >> tetta;
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++) {
+            Aiter(i, j) = (A(i, j) / A(i, i)) * tetta;
+            if(i == j) {
+                Aiter(i, j) = 1 - tetta;
+            }
+        }
+
+    }
+
+    std::cout << Aiter;
+    std::cout << B;
     // fuck off cycles
 //    for(int i = 0; i < N; i++){
 //        for(int j = 0; j < N; j++){ // n=m
@@ -20,7 +41,5 @@ int main() {
 //        }
 //        std::cout << std::endl;
 //    }
-    std::cout << A;
-    At = A.transpose();
-    Acomposion = At*A;
+    // std::cout << A;
 }
