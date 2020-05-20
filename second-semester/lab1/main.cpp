4cf65d154abebd17271078c1ff784de225195b65
whoisprivalov <nikita.karatsev@gmail.com>
2020-05-20 21:07:19 +0600

    [~]delete comments in lab1

--- second-semester/lab1/main.cpp
+++ second-semester/lab1/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <matrix.h>
 #include <cmath>
+#include <sys/time.h>
+#include <time.h>
 
+#define NS_IN_S  1E9
 //Demention of matrix
 #define N 4
 
@@ -29,36 +32,31 @@
 1.2
 -3.7
 -----------------------
-Answer: x1=
-x2=
-x3=
-x4=
+Answer:
+-0.201885
+0.160131
+0.262423
+-0.288252
 */
 
 // Instert matrix
-void insert_matrix(Matrix& A, Matrix& A_t, Matrix& B, Matrix& X);
+void insert_matrix(Matrix& A, Matrix& B);
 
 // Normalization ||x||(infinity) = max(|xi|) by i
-double norm_inf(Matrix vector_plus, Matrix vector);
+double norm_inf(Matrix);
 
 int main() {
+    // Time
+    struct timespec start, stop, duration;
     // Source matrix
     Matrix A(N, N);
-    // Transpose matrix
-    Matrix A_t(N, N);
-    // Vector of A*A(transpose)*R
-    Matrix AAr(N, N);
-    // Matrix for iteration x(i) = b - x1 - x2 - ... - x(i-1) - x(i+1) - ... - x(n)
+    // Matrix A * vector R
     Matrix Ar(N, 1);
-    //Matrix A_iteration(N, N);
     // Vector B(of free member)
     Matrix B(N, 1);
     // Now iteration
     Matrix X(N, 1);
-    // Next iteration
-    Matrix X_next(N, 1);
-    // Iteration parametr
-    //double tetta = 1.4;
+    // Residual vector
     Matrix R(N,1);
     // Alpha
     double alpha_numerator;
@@ -72,13 +70,7 @@ int main() {
     int iteration = 0;
 
     // Insert matrix
-    insert_matrix(A, A_t, B, X);
-
-//    for (int i = 0; i < N; i++) {
-//        for (int j = 0; j < N + 1; j++) {
-//
-//        }
-//    }
+    insert_matrix(A, B);
 
     //Input epsilon
     std::cout << "Input Epsilon: ";
@@ -90,77 +82,50 @@ int main() {
     if (init == 0) {
         for (int i = 0; i < N; i++) {
             X(i, 0) = 0;
-            X_next(i, 0) = 0;
         }
     } else {
         for (int i = 0; i < N; i++) {
             X(i, 0) = 1;
-            X_next(i, 0) = 0;
         }
     }
-
-    //first iteration
-    //B = B.transpose();
-    R = B - (A*X);
-    // R -= B;
-    Ar = A*R;
-    // AAr = A*A_t*R;
-    for(int i = 0; i < N; i++){
-        alpha_numerator += R(i,0)*R(i,0);
-        alpha_denominator += Ar(i,0)*R(i, 0);
-    }
-    alpha = alpha_numerator/alpha_denominator;
-    X_next = X + alpha*R;
-
-//    for(int l = 0; l < N; l++){
-//        for(int m = 0; m < N; m++) {
-//            K_plus(l, 0) += (A_k_iter(l, m) * K(m, 0));
-//        }
-//        K_plus(l, 0) += A_k_iter(l, N);
-//    }
 
     std::cout << "Basic matrix:" << std::endl;
     std::cout << A;
     std::cout << "Basic vector B:" << std::endl;
     std::cout << B;
-    std::cout << "Basic vector R:" << std::endl;
-    std::cout << R;
-    std::cout << "Basic vector Ar:" << std::endl;
-    std::cout << Ar;
-    std::cout << "Basic vector AAr:" << std::endl;
-    std::cout << AAr;
-    std::cout << "Alpha:" << std::endl;
-    std::cout << alpha;
     std::cout << "===================================\n";
-    std::cout << "N = " << iteration << std::endl;
-    std::cout << "X(0):\n" << X << std::endl;
-    std::cout << "X(1):\n"<< X_next << std::endl;
-    iteration++;
-    // while ||x^(K+1) - x^(k)||(inf) > epsilon do:
-    while(norm_inf(X_next, X) > epsilon){
-        X = X_next;
-        R = B - (A*X);
-        // R -= B;
+    // while ||R||(inf) > epsilon do:
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+    R = B - (A * X);
+    while(norm_inf(R) > epsilon){
         Ar = A*R;
-        // AAr = A*A_t*R;
         for(int i = 0; i < N; i++){
             alpha_numerator += R(i,0)*R(i,0);
             alpha_denominator += Ar(i,0)*R(i, 0);
         }
         alpha = alpha_numerator/alpha_denominator;
-        X_next = X + alpha*R;
-        alpha = 0.5*(alpha_numerator/alpha_denominator);
+        X = X + alpha*R;
+        R = B - (A*X);
         iteration++;
-        std::cout << "===================================\n";
-        std::cout << "N = " << iteration << std::endl;
-        std::cout << "X(" << iteration - 1 << "):\n" << X << std::endl;
-        std::cout << "X(" << iteration << ")\n"<< X_next << std::endl;
     }
-    std::cout << "\nVector x(in " << iteration << " iteration) is:\n" << X_next << "\ndifference(in normal):\n" << norm_inf(X_next, X);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
+    if ((stop.tv_nsec - start.tv_nsec) < 0)
+    {
+        duration.tv_sec = stop.tv_sec - start.tv_sec - 1;
+        duration.tv_nsec = NS_IN_S + stop.tv_nsec - start.tv_nsec;
+    }
+    else
+    {
+        duration.tv_sec = stop.tv_sec - start.tv_sec;
+        duration.tv_nsec = stop.tv_nsec - start.tv_nsec;
+    }
+    double result = duration.tv_sec + duration.tv_nsec / NS_IN_S;
+    std::cout << "\nVector x(in " << iteration << " iteration) is:\n" << X;
+    std::cout << "TIME: " << result;
     return 0;
 }
 
-void insert_matrix(Matrix& A, Matrix& A_t, Matrix& B, Matrix& X){
+void insert_matrix(Matrix& A, Matrix& B){
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             std::cout << "A[" << i + 1 << "][" << j + 1 << "]: ";
@@ -171,31 +136,14 @@ void insert_matrix(Matrix& A, Matrix& A_t, Matrix& B, Matrix& X){
         std::cout << "B[" << i + 1 << "]: ";
         std::cin >> B(i,0);
     }
-//    for(int i = 0; i < N; i++){
-//        std::cout << "X[" << i + 1 << "]: ";
-//        std::cin >> X(i,0);
-//    }
-    //X(0,0) = 0.3;
-    //X(1,0) = -0.05;
-    //X(2,0) = -0.2;
-    //X(3, 0) = 0.3;
-    A_t = A.transpose();
 }
 
-double norm_inf(Matrix vector_plus, Matrix vector){
+double norm_inf(Matrix vector){
     double max = 0;
-    Matrix difference(N,1);
-    // Find x^(k+1) - x^(k) (k-s iteration)
+    max = std::abs(vector(0,0));
     for(int i = 0; i < N; i++){
-        difference(i,0) = std::abs(vector_plus(i, 0)) - std::abs(vector(i, 0));
-    }
-    std::cout << "difference:\n" << difference;
-
-    //find max element of normal
-    max = std::abs(difference(0,0));
-    for(int i = 0; i < N; i++){
-        if(max < std::abs(difference(i, 0))){
-            max = std::abs(difference(i, 0));
+        if(max < std::abs(vector(i, 0))){
+            max = std::abs(vector(i, 0));
         }
     }
     return max;
